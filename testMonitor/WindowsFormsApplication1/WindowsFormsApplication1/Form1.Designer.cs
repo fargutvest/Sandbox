03c4c8c134361d42b05fa71432e5e5806a370f85
fargutvest <fargutvest@gmail.com>
2014-11-08 10:23:36 +0300

    testMonitor change

---
+++ testMonitor/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 14);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1, 1);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(115, 99);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ testMonitor/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bgw = new BackgroundWorker();
+        AutoResetEvent are = new AutoResetEvent(false);
+        Graphics graphics;
+        Task task;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bgw.DoWork += bgw_DoWork;
+
+
+            //task = new Task(taskExecute);
+            //task.Start();
+
+        }
+
+        void taskExecute()
+        {
+            BeginInvoke(new Action(() =>
+            {
+                //this.Size = SystemInformation.PrimaryMonitorSize;
+                this.Location = new Point(0, 0);
+
+                AutoResetEvent are = new AutoResetEvent(false);
+                int delay = 10;
+
+                List<Color> colors = new List<Color>();
+                colors.Add(Color.Red);
+                colors.Add(Color.Green);
+                colors.Add(Color.Blue);
+                colors.Add(Color.Black);
+                colors.Add(Color.White);
+
+
+                int count = 0;
+                while (true)
+                {
+                    count++;
+                    if (count == colors.Count)
+                        count = 0;
+
+                    this.BackColor = colors[count];
+
+                    are.WaitOne(delay);
+                    //are.WaitOne();
+                }
+            }));
+        }
+
+
+
+        void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            /*BeginInvoke(new Action(() =>
+            {*/
+            //this.Size = SystemInformation.PrimaryMonitorSize;
+            //this.Location = new Point(0, 0);
+
+            AutoResetEvent are = new AutoResetEvent(false);
+            int delay = 1;
+
+            List<Color> colors = new List<Color>();
+            colors.Add(Color.Red);
+            colors.Add(Color.Green);
+            colors.Add(Color.Blue);
+            colors.Add(Color.Black);
+            colors.Add(Color.White);
+
+
+            int count = 0;
+
+            Bitmap _bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Random rnd = new Random();
+            graphics = Graphics.FromImage(_bitmap);
+
+            while (true)
+            {
+                _bitmap.SetPixel(0, 0, Color.FromArgb(0, rnd.Next(0, 255), 0));
+                graphics.DrawImage((Image)_bitmap, 0, 0);
+
+                BeginInvoke(new Action(() =>
+                {
+                    try
+                    { 
+                        pictureBox1.Image = _bitmap; 
+                    }
+                    catch (Exception) { }
+                }));
+
+
+                /*
+                count++;
+                if (count == colors.Count)
+                    count = 0;
+         
+                this.BackColor = colors[count];
+                this.Refresh();
+                 */
+                are.WaitOne(delay);
+
+
+            }
+            /* }));*/
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            are.Set();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            bgw.RunWorkerAsync();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
