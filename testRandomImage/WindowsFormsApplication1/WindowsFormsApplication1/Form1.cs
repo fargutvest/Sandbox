e26ab5ca958037a0bc67cb344ccf72409bed5b5b
Myslicki <Myslicki@adani.by>
2015-01-14 20:16:50 +0300

    add

---
+++ testRandomImage/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            test();
+        }
+
+        void test()
+        {
+            Random rand = new Random();
+            Bitmap bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    int grey = rand.Next(255);
+                    bitmap.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+            //Graphics graphics = Graphics.FromImage(bitmap);
+
+
+        }
+    }
+}
---
+++ testUseCSharpDll_InDelphi/CSharpDll/testWin32/testWin32/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace testWin32
+{
+
+    [ComVisible(true)]
+    public interface IMyInterface
+    {
+        int Sum(int x, int y);
+        string AppendText(string text);
+    }
+
+
+    [ComVisible(true), ClassInterface(ClassInterfaceType.None)]
+    public class TestClass : IMyInterface
+    {
+
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public string AppendText(string Text)
+        {
+            StringBuilder sb = new StringBuilder(Text);
+            sb.Append(DateTime.Now.Year);
+            return sb.ToString();
+        }
+
+    }
+}
