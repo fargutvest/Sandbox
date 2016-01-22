96314b007a396c0f245d1afc71998121b6211be4
Gennady Myslitsky <Fargutvest@gmail.com>
2016-01-22 14:11:47 +0300

    Few clean C# code

--- C#/Beep system speaker/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ C#/Beep system speaker/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-	using System.Collections.Generic;
-	using System.ComponentModel;
-	using System.Data;
-	using System.Windows.Forms;
-	using System.Runtime.InteropServices;
-	  
-	namespace Sample
-	{
-	    public partial class Form1 : Form
-	    {
-	        public Form1()
-	        {
-	            InitializeComponent();
-            }
-	  
-	        [DllImport("kernel32.dll")]
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
-	        public static extern bool Beep(int BeepFreq, int BeepDuration);
-	  
-	        private void button1_Click(object sender, EventArgs e)
-	        {
-	            Beep(5000, 1000);
-	        }
+namespace Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-	    }
-	}
+        [DllImport("kernel32.dll")]
+
+        public static extern bool Beep(int BeepFreq, int BeepDuration);
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Beep(5000, 1000);
+        }
+    }
+}
