a6647075bffb70de3e5cb9088f19aa9f49f5a50c
Fargutwest <fargutvest@gmail.com>
2020-08-15 15:25:08 +0300

    Added sandbox for IEnumerable

---
+++ C#/IEnumerable/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SandboxIEnumerable
+{
+    public class Cat
+    {
+        public string Name { get; private set; }
+        public Cat(string name)
+        {
+            Name = name;
+        }
+    }
+}
+
