75d3da314180e9527e59156d3f53bd6e2ebfd83d
Henadzi Myslitski <henadzi.myslitski@elliemae.com>
2020-08-15 15:25:08 +0300

    Added sandbox for IEnumerable

---
+++ C#/IEnumerable/Cats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SandboxIEnumerable
+{
+    public class Cats : IEnumerable
+    {
+        private List<Cat> _catsList;
+
+        public Cats()
+        {
+            _catsList = new List<Cat>();
+
+            
+            for (int i = 0; i < 5; i++)
+            {
+                _catsList.Add(new Cat($"I am cat number {i + 1}")); 
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _catsList.GetEnumerator();
+        }
+    }
+}
---
+++ C#/IEnumerable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace SandboxIEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cats = new Cats();
+            var catIEnumerable = cats.Cast<Cat>();
+            var catList = catIEnumerable.ToList();
+
+            
+            foreach (var item in catIEnumerable)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine(new string('-', 50));
+
+            catList.ForEach(_ => Console.WriteLine(_.Name));
+
+            Console.ReadKey();
+        }
+    }
+}
