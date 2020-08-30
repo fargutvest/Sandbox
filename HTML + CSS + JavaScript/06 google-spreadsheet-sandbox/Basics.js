d860c1d8a6e33d0ac268aa37498df005dea6e712
Henadzi Myslitski <fargutvest@gmail.com>
2020-08-30 13:03:29 +0000

    Added example of writing into spreadsheet. Required quickstart-1554057009071-aa2574f181ba.json. To get this json file read instruction: https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication?id=service-account

--- HTML + CSS + JavaScript/06 google-spreadsheet-sandbox/Basics.js
+++ HTML + CSS + JavaScript/06 google-spreadsheet-sandbox/Basics.js
@@ -1,11 +1,27 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+const credentials = require('./config/quickstart-1554057009071-aa2574f181ba.json');
+const spreadsheetID = '1AzLrSQ2-L-AKOphu3ePNqbAzujkLTyeCKXibMjRYqV8';
 
 start();
 async function start() {
-const doc = new GoogleSpreadsheet('11QLw-rY3z186hybTlI9QkXDvgLGz39o3_15HOXryqkw');
-doc.useApiKey('AIzaSyANRAmPJFTjvI2lxfJpq82rd4SHtpBdKY0');
-await doc.loadInfo();
-console.log("Hello world");
+console.log("Hello google-spreadsheet");
 console.log('https://www.npmjs.com/package/google-spreadsheet')
+
+const doc = new GoogleSpreadsheet(spreadsheetID);
+//doc.useApiKey('AIzaSyANRAmPJFTjvI2lxfJpq82rd4SHtpBdKY0');
+console.log(credentials);
+doc.useServiceAccountAuth(credentials);
+await doc.loadInfo();
 console.log(doc.title)
+
+var currentdate = new Date(); 
+var datetime =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+const newSheet = await doc.addSheet({ title: datetime });
+console.log(datetime);
 }
