ba3665afd64ed4b32e90eb0863afbeb21acdee1c
Fargutvest <fargutvest@gmail.com>
2020-10-01 04:19:09 +0300

    minor viyar parser

--- .NET/ViyarParser/ViyarParser/Program.cs
+++ .NET/ViyarParser/ViyarParser/Program.cs
@@ -1,20 +1,21 @@
-﻿using HtmlAgilityPack;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
-namespace ConsoleApp9
+namespace ViyarParser
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             var pages = new List<string>();
 
             var myClient = new WebClient();
+
             for (int i = 0; i < 7; i++)
             {
                 var response = myClient.OpenRead($"https://viyar.by/catalog/dsp_1/page-{i + 1}/?view=60");
@@ -26,19 +27,16 @@ namespace ConsoleApp9
             var codeToImageMappings = new Dictionary<string, string>();
 
             foreach (var page in pages)
-	    {
-                var doc  = new HtmlDocument();
+            {
+                var doc = new HtmlDocument();
                 doc.LoadHtml(page);
 
                 foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//div[contains(@class, 'col-xs-12 col-sm-6 col-md-6 col-lg-4 product_prewiew-wrapper')]"))
                 {
                     var innerHtml = node.InnerHtml;
-                    var a = innerHtml.Substring(innerHtml.IndexOf("/upload/resize_cache/photos/300_300_1/"));
-                    var imgUrl = "https://viyar.by/" + a.Substring(0, a.IndexOf(".jpg") + 4);
+                    var imgUrl = "https://viyar.by" + new Regex("src\\s*=\\s*\"(.+?)\"").Match(innerHtml).ToString().Replace("\"","").Replace("src=","");
 
-
-                    a = innerHtml.Substring(innerHtml.IndexOf("<span>Код товара: ") + 18);
-                    var code  = a.Substring(0, a.IndexOf("</span>"));
+                    var code = new Regex("<span>Код товара:(.+?)</span>").Match(innerHtml).ToString().Replace("<span>Код товара: ", "").Replace("</span>", "");
                     codeToImageMappings.Add(code, imgUrl);
                 }
             }
@@ -50,16 +48,12 @@ namespace ConsoleApp9
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var values = line.Split(',');
+                    var values = line.Split(',').ToList();
                     var code = values[1];
                     codeToImageMappings.TryGetValue(code, out var imgUrl);
-                    var withUrl = values.ToList();
-                    withUrl.Add(imgUrl);
-
-                    remainsList.Add(withUrl.ToArray());
+                    remainsList.Add(new string[] { line, imgUrl });
                 }
             }
-
 
             var resultHtml = "<!DOCTYPE html>" +
                              "<html>" +
@@ -70,7 +64,7 @@ namespace ConsoleApp9
             {
                 resultHtml += "<li>" +
                     $"<img src='{item.Last()}' width='300'>" +
-                    $"{string.Join(",", item)}" +
+                    $"{item.First()}" +
                     "</li>";
             }
 
@@ -78,13 +72,18 @@ namespace ConsoleApp9
                 "</body>" +
                 "</html>";
 
+            var resultFilePath = "result.html";
+            if (File.Exists(resultFilePath))
+            {
+                File.Delete(resultFilePath);
+            }
 
-            using (StreamWriter file = new StreamWriter(@"result.html", true))
+            using (StreamWriter file = new StreamWriter(resultFilePath, true))
             {
                 file.WriteLine(resultHtml);
             }
         }
-
     }
-
 }
+
+
