31c623a643b1ffb5e86c3f3ceeb0bcb3b84ee00b
Henadzi.Myslitski <fargutvest@gmail.com>
2020-10-04 21:42:40 +0300

    Viyar parser enchancement

---
+++ .NET/ViyarParser/ViyarParser/EntryModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Windows;
+
+namespace ViyarParser
+{
+    public class EntryModel
+    {
+        public string Code { get; set; }
+        public string Nomenklature { get; set; }
+        public Size Characteristic { get; set; }
+        public string Thikness { get; set; }
+        public double Count { get; set; }
+        public string ImageUrl { get; set; }
+        public string Price { get; set; }
+        public string Cost { get; set; }
+    }
+}
