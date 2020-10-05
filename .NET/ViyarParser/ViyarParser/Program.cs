d6ccebb36d6c80c422f1fe39e1f10691a609831a
Fargutvest <fargutvest@gmail.com>
2020-10-05 13:08:28 +0300

    parser viyar: count of pages now is not hardcoded

--- .NET/ViyarParser/ViyarParser/Program.cs
+++ .NET/ViyarParser/ViyarParser/Program.cs
@@ -15,41 +15,46 @@ namespace ViyarParser
     {
         static void Main(string[] args)
         {
-            var pages = new List<string>();
-
-            var myClient = new WebClient();
-
-            for (int i = 0; i < 7; i++)
-            {
-                var response = myClient.OpenRead($"https://viyar.by/catalog/dsp_1/page-{i + 1}/?view=60");
-                var reader = new StreamReader(response);
-                pages.Add(reader.ReadToEnd());
-                response.Close();
-            }
-
             var codeToImageMappings = new Dictionary<string, string>();
             var codeToPriceMappings = new Dictionary<string, string>();
 
-            foreach (var page in pages)
+            var myClient = new WebClient();
+            HtmlNodeCollection nodes = null;
+            var pageNum = 1;
+            var isLastPageAchieved = false;
+            do
             {
+                Console.Write('.');
+                var response = myClient.OpenRead($"https://viyar.by/catalog/dsp_1/page-{pageNum}/?view=60");
+                var reader = new StreamReader(response);
+                var page = reader.ReadToEnd();
+                response.Close();
+
                 var doc = new HtmlDocument();
                 doc.LoadHtml(page);
-
-                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//div[contains(@class, 'col-xs-12 col-sm-6 col-md-6 col-lg-4 product_prewiew-wrapper')]"))
+                nodes = doc.DocumentNode.SelectNodes("//div[contains(@class, 'col-xs-12 col-sm-6 col-md-6 col-lg-4 product_prewiew-wrapper')]");
+                foreach (HtmlNode node in nodes)
                 {
+                    Console.Write('o');
                     var innerHtml = node.InnerHtml;
-                    var imgUrl = "https://viyar.by" + new Regex("src\\s*=\\s*\"(.+?)\"").Match(innerHtml).ToString().Replace("\"","").Replace("src=","");
+                    var imgUrl = "https://viyar.by" + new Regex("src\\s*=\\s*\"(.+?)\"").Match(innerHtml).ToString().Replace("\"", "").Replace("src=", "");
 
                     var code = new Regex("<span>Код товара:(.+?)</span>").Match(innerHtml).ToString().Replace("<span>Код товара: ", "").Replace("</span>", "");
                     var price = new Regex("<span class=\"price\">(.+?)</span>").Match(innerHtml).ToString().Replace("<span class=\"price\">", "").Replace("</span>", "");
                     var strike = new Regex("<strike class=\"price-odd\">(.+?)</strike>").Match(price).ToString();
                     price = string.IsNullOrEmpty(strike) ? price : price.Replace(strike, "");
+                    if (codeToImageMappings.ContainsKey(code))
+                    {
+                        isLastPageAchieved = true;
+                        break;
+                    }
                     codeToImageMappings.Add(code, imgUrl);
                     codeToPriceMappings.Add(code, price);
                 }
-            }
+                pageNum++;
+            } while (isLastPageAchieved == false);
 
-            var fileName = "stebeneva1.xls";
+            var fileName = "stebeneva3.xls";
             new WebClient().DownloadFile("https://viyar.by/upload/ex_files/stebeneva.xls", fileName);
 
             var remainsList = new List<EntryModel>();
@@ -67,6 +72,7 @@ namespace ViyarParser
                 int row = 2;
                 while (xlRange.Cells[row, 2].Value2 != null)
                 {
+                    Console.Write('*');
                     var entry = new EntryModel()
                     {
                         Code = xlRange.Cells[row, 2].Value2.ToString(),
@@ -80,11 +86,11 @@ namespace ViyarParser
                     codeToPriceMappings.TryGetValue(entry.Code, out var price);
                     entry.ImageUrl = imgUrl;
                     entry.Price = price;
-                    
-                    entry.Cost = entry.Price!= null ? (entry.Count / (2070 * 2800 / (double)1000000) * double.Parse(entry.Price.Replace(" руб/лист", ""))).ToString("0.##") + "руб" : null;
+
+                    entry.Cost = entry.Price != null ? (entry.Count / (2070 * 2800 / (double)1000000) * double.Parse(entry.Price.Replace(" руб/лист", ""))).ToString("0.##") + "руб" : null;
                     remainsList.Add(entry);
                     row++;
-                } 
+                }
             }
             finally
             {
@@ -100,11 +106,11 @@ namespace ViyarParser
 
             var resultDoc = new HtmlDocument();
 
-            var grouped = remainsList.GroupBy(_ => _.Nomenklature).ToDictionary(_=> _.Key, _=>_.OrderBy(x=>x.Count).ToList());
+            var grouped = remainsList.GroupBy(_ => _.Nomenklature).ToDictionary(_ => _.Key, _ => _.OrderBy(x => x.Count).ToList());
 
             foreach (var item in grouped)
             {
-                
+                Console.Write('"');
                 resultDoc.DocumentNode.AppendChild(HtmlNode.CreateNode($@"<li>
                                                                             <table>
                                                                                 <tr>
@@ -113,7 +119,7 @@ namespace ViyarParser
                                                                                     </td>
                                                                                     <td>
                                                                                         <div><b>{item.Value.First().Code}</b> - {item.Key}</div>
-                                                                                        {string.Join("", item.Value.Select(_=> $"<div>{_.Characteristic.ToString().Replace(",","х")} ({_.Count}) - <b>{_.Cost}</b> ({_.Price})</div>").ToList())}
+                                                                                        {string.Join("", item.Value.Select(_ => $"<div>{_.Characteristic.ToString().Replace(",", "х")} ({_.Count}) - <b>{_.Cost}</b> ({_.Price})</div>").ToList())}
                                                                                     </td>
                                                                                 </tr>
                                                                             </table>
