5c9bdeddfc44f66911080579f021cdd122cf5353
Fargutwest <fargutvest@gmail.com>
2020-10-10 13:07:47 +0300

    Moived all tryouts under Sandbox Soilution via script: dotnet new sln Get-ChildItem -Recurse *.csproj | ForEach { dotnet sln add $_.FullName } https://stackoverflow.com/questions/414309/generate-solution-file-from-list-of-csproj

---
+++ dotnet/SandboxSolution/Alternate browser/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.CausesValidation = false;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 262);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ dotnet/SandboxSolution/Alternate browser/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Skybound.Gecko.GeckoWebBrowser browser;
+        public Form1()
+        {
+            InitializeComponent();
+            string pach = $@"{Environment.CurrentDirectory}\..\..\..\..\doc\xulrunner-1.9.2.en-US.win32.sdk\xulrunner-sdk\bin\";
+            Skybound.Gecko.Xpcom.Initialize(pach);
+            browser = new Skybound.Gecko.GeckoWebBrowser();
+            browser.Parent = this.panel1;
+            browser.Dock = DockStyle.Fill;
+            browser.Click += new EventHandler(browser_Click);
+            browser.MouseClick += new MouseEventHandler(browser_MouseClick);
+        }
+
+        void browser_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point p = browser.PointToClient(Cursor.Position);
+        }
+
+        void browser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser.Navigate("http://google.com");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Skybound.Gecko.GeckoElementCollection el = browser.Document.GetElementsByTagName("button");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_Click(this, null);
+        }
+    }
+}
