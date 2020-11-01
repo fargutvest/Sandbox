eab58257c216b837e8d96a117c56041250a339ac
Fargutwest <fargutvest@gmail.com>
2020-11-01 17:03:34 +0300

    moved PUMLConvertor repository under Sandbox

---
+++ dotnet/PUMLConvertor/PUMLConverter/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace PUMLConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext;
+            var vm = dc as MainWindowViewModel;
+            vm.ToPUML();
+        }
+    }
+}
---
+++ dotnet/PUMLConvertor/PUMLConverter/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PUMLConverter.Annotations;
+
+namespace PUMLConverter
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public string Source { get; set; }
+        public string Result { get; set; }
+
+        public void ToPUML()
+        {
+            Result = Source.Replace("\r\n", "\\n");
+            OnPropertyChanged(nameof(Result));
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
