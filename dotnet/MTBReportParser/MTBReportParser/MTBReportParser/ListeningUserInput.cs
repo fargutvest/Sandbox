a09a34fd4bd7dcd15c0415bc60b98db631a28b07
Fargutwest <fargutvest@gmail.com>
2021-01-16 16:49:25 +0300

    MTBReportParser console interactive improvements

---
+++ dotnet/MTBReportParser/MTBReportParser/MTBReportParser/ListeningUserInput.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyConsole
+{
+    public class ListeningUserInput
+    {
+        private List<string> _userInputHistory = new List<string>();
+        private string here = ">";
+
+        public string Listen()
+        {
+            Console.Write(here);
+
+            ConsoleKeyInfo keyInfo;
+            Console.TreatControlCAsInput = true;
+            var historyCurrent = 0;
+
+            var chars = new List<char>();
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                if ((keyInfo.Key == ConsoleKey.UpArrow || (keyInfo.Key == ConsoleKey.DownArrow && historyCurrent > 0)) && _userInputHistory.Count > 0)
+                {
+                    if (keyInfo.Key == ConsoleKey.DownArrow)
+                    {
+                        historyCurrent--;
+                    }
+                    else if (keyInfo.Key ==  ConsoleKey.UpArrow && historyCurrent < _userInputHistory.Count - 1)
+                    {
+                        historyCurrent++;
+                    }
+
+                    chars.Clear();
+                    chars.AddRange(_userInputHistory[historyCurrent].ToCharArray());
+                    ClearCurrentConsoleLine();
+                    Console.Write($"\r{here}{new string(chars.ToArray())}");
+                }
+                else if ((keyInfo.Key == ConsoleKey.LeftArrow && Console.CursorLeft > here.Length) || (keyInfo.Key == ConsoleKey.RightArrow && Console.CursorLeft < chars.Count + here.Length))
+                {
+                    Console.SetCursorPosition(keyInfo.Key == ConsoleKey.LeftArrow ? Console.CursorLeft - 1 : Console.CursorLeft + 1, Console.CursorTop);
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace && chars.Count > 0 && Console.CursorLeft - here.Length - 1 >= 0)
+                {
+                    var curPos = Console.CursorLeft - here.Length - 1;
+                    chars.RemoveAt(curPos);
+                    ClearCurrentConsoleLine();
+                    Console.Write($"\r{here}{new string(chars.ToArray())}");
+                    Console.SetCursorPosition(curPos + 1, Console.CursorTop);
+                }
+                else if (keyInfo.Key == ConsoleKey.Delete && chars.Count > 0 && Console.CursorLeft < chars.Count + here.Length)
+                {
+                    var curPos = Console.CursorLeft - here.Length;
+                    chars.RemoveAt(curPos);
+                    ClearCurrentConsoleLine();
+                    Console.Write($"\r{here}{new string(chars.ToArray())}");
+                    Console.SetCursorPosition(curPos + 1, Console.CursorTop);
+                }
+                else if (IsDisplayable(keyInfo.Key))
+                {
+                    var curPos = Console.CursorLeft;
+                    chars.Insert(curPos - here.Length, keyInfo.KeyChar);
+                    Console.Write($"\r{here}{new string(chars.ToArray())}");
+                    Console.SetCursorPosition(curPos + 1, Console.CursorTop);
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Enter);
+
+            var userInput = new string(chars.ToArray());
+            if (string.IsNullOrEmpty(userInput) == false && _userInputHistory.Contains(userInput) == false)
+            {
+                _userInputHistory.Add(userInput);
+            }
+
+            Console.WriteLine();
+            return userInput;
+        }
+
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+
+
+        private bool IsDisplayable(ConsoleKey key)
+        {
+            return (key >= ConsoleKey.D0 && key <= ConsoleKey.Z) ||
+                   (key >= ConsoleKey.NumPad0 && key <= ConsoleKey.Divide) ||
+                   (key >= ConsoleKey.Oem1 && key <= ConsoleKey.Oem102);
+        }
+    }
+}
--- dotnet/MTBReportParser/MTBReportParser/MTBReportParser/Program.cs
+++ dotnet/MTBReportParser/MTBReportParser/MTBReportParser/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
+using System.Linq;
+using System.Configuration;
+using System.Collections.Generic;
 using HtmlAgilityPack;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 using System.Collections;
+using Newtonsoft.Json;
+using MyConsole;
+using System.Globalization;
+using System.Threading.Tasks;
 
 namespace MTBReportParser
 {
@@ -15,77 +19,67 @@ namespace MTBReportParser
     {
         static void Main(string[] args)
         {
-            var operations = new List<Operation>();
-
-            foreach (var item in Directory.GetFiles(args[0]))
-            {
-                var raw = File.ReadAllText(item);
-                var days = OpenNode(raw).SelectNodes("//div[contains(@class, 'operations-by-day-row payment-history')]").ToList();
-
-                var listNodes = new List<Operation>();
-
-                foreach (var node in days.Select(_ => OpenNode(_.InnerHtml)))
-                {
-                    var date = node.SelectSingleNode("//div[contains(@class, 'day-header')]").InnerText;
-                    var dayOperations = node.SelectNodes("//div[contains(@class, 'operation-row-wrap')]");
-
-                    foreach (var operation in dayOperations.Select(_ => OpenNode(_.InnerHtml)))
-                    {
-                        var complete = operation.SelectSingleNode("//div[contains(@class, 'operation-status complete')]");
-                        var notCompleted = operation.SelectSingleNode("//div[contains(@class, 'operation-status operation-not-completed')]");
-                        var operationIn = operation.SelectSingleNode("//div[contains(@class, 'operation-status operation-in')]");
-                        var status = GetStatus(complete, notCompleted, operationIn);
-                        var title = operation.SelectNodes("//div[contains(@class, 'operation-title')]")[1].InnerText;
-                        var place = operation.SelectSingleNode("//div[contains(@class, 'operation-place')]").InnerText;
-                        var time = operation.SelectSingleNode("//div[contains(@class, 'operation-time')]").InnerText;
-                        var currencyNode = operation.SelectSingleNode("//div[contains(@class, 'operation-sum-currency-main')]");
-                        var amount = currencyNode != null ? double.Parse($"{currencyNode.ChildNodes[0].InnerText}{currencyNode.ChildNodes[1].InnerText}".Replace(" ", "").Replace(",", ".")) : 0;
-                        var currency = currencyNode?.ChildNodes[2]?.InnerText;
-
-                        var dateTime = DateTime.Parse($"{date.Replace(",", "").Replace("года", "")} {time}", new CultureInfo("ru-RU"));
-                        listNodes.Add(new Operation() { DateTime = dateTime, Title = title, Place = place,  Amount = amount, Currency = currency , Status = status });
-                    }
-                }
-
-                listNodes.Reverse();
-                operations.AddRange(listNodes);
-            }
-
-
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
+            var operations = new List<Operation>();
+            operations.AddRange(Load());
             Console.WriteLine(string.Join(Environment.NewLine, operations.Where(_ => _.Status != Status.Decline).Select(_ => _.ToString())));
 
+            var console = new ListeningUserInput();
+            var help = "help";
+            var exit = "exit";
             while (true)
             {
-                var input = Console.ReadLine();
+                var input = console.Listen();
 
-                try
+                if (input == help)
                 {
-                    var scriptOptions = ScriptOptions.Default.AddImports("System.Linq").AddImports("System").AddReferences(typeof(Enumerable).Assembly);
-                    var result = CSharpScript.EvaluateAsync(
-                               input,
-                               scriptOptions, globals: new Global() { _ = operations }).Result;
-
-
-                    if (typeof(IEnumerable).IsAssignableFrom(result.GetType()))
-                    {
-                        Console.WriteLine(string.Join(Environment.NewLine, (result as IEnumerable<object>).Select(_ => _.ToString())));
-                    }
-                    else
-                    {
-                        Console.WriteLine(result);
-                    }
-
+                    Console.WriteLine($"{Environment.NewLine}{new string('*', Console.WindowWidth - 1)}");
+                    ;
+                    Console.WriteLine(
+                        $"Use '_' to access collection." +
+                        $"{Environment.NewLine}Last item as json:" +
+                        $"{Environment.NewLine}{JsonConvert.SerializeObject(operations.Last(), Formatting.Indented)}" +
+                        $"{Environment.NewLine}Types:" +
+                        $"{Environment.NewLine}{string.Join(Environment.NewLine, typeof(Operation).GetProperties(System.Reflection.BindingFlags.Public| System.Reflection.BindingFlags.Instance).Select(_ => $"{_.PropertyType} {_.Name}"))}");
+                    Console.WriteLine($"{new string('*', Console.WindowWidth-1)}{Environment.NewLine}");
                 }
-                catch (Exception ex)
+                else if (input == exit)
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Bye");
+                    Task.Delay(300).Wait();
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        var scriptOptions = ScriptOptions.Default.AddImports("System.Linq").AddImports("System").AddReferences(typeof(Enumerable).Assembly);
+                        var result = CSharpScript.EvaluateAsync(
+                                   input,
+                                   scriptOptions, globals: new Global() { _ = operations }).Result;
+
+
+                        if (typeof(IEnumerable).IsAssignableFrom(result.GetType()))
+                        {
+                            Console.WriteLine(string.Join(Environment.NewLine, (result as IEnumerable<object>).Select(_ => _.ToString())));
+                        }
+                        else
+                        {
+                            Console.WriteLine(result);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine($"Input '{help}' to get more info.");
+                    }
                 }
             }
         }
 
-
+      
 
         private static HtmlNode OpenNode(string html)
         {
@@ -112,6 +106,53 @@ namespace MTBReportParser
             {
                 return Status.Unknown;
             }
+        }
+
+        private static List<Operation> Load()
+        {
+            var result = new List<Operation>();
+            var files = Directory.GetFiles(ConfigurationManager.AppSettings["Folder"]);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                double percent = (i + 1) / (double)files.Length * 100;
+                Console.Write($"\rLoading {percent.ToString("F0")}%");
+
+                var raw = File.ReadAllText(files[i]);
+                var days = OpenNode(raw).SelectNodes("//div[contains(@class, 'operations-by-day-row payment-history')]").ToList();
+
+                var listNodes = new List<Operation>();
+
+                foreach (var node in days.Select(_ => OpenNode(_.InnerHtml)))
+                {
+                    var date = node.SelectSingleNode("//div[contains(@class, 'day-header')]").InnerText;
+                    date = date.Contains("Сегодня") ? date.Replace("Сегодня", "") : date;
+                    var dayOperations = node.SelectNodes("//div[contains(@class, 'operation-row-wrap')]");
+
+                    foreach (var operation in dayOperations.Select(_ => OpenNode(_.InnerHtml)))
+                    {
+                        var complete = operation.SelectSingleNode("//div[contains(@class, 'operation-status complete')]");
+                        var notCompleted = operation.SelectSingleNode("//div[contains(@class, 'operation-status operation-not-completed')]");
+                        var operationIn = operation.SelectSingleNode("//div[contains(@class, 'operation-status operation-in')]");
+                        var status = GetStatus(complete, notCompleted, operationIn);
+                        var title = operation.SelectNodes("//div[contains(@class, 'operation-title')]")[1].InnerText;
+                        var place = operation.SelectSingleNode("//div[contains(@class, 'operation-place')]").InnerText;
+                        var time = operation.SelectSingleNode("//div[contains(@class, 'operation-time')]").InnerText;
+                        var currencyNode = operation.SelectSingleNode("//div[contains(@class, 'operation-sum-currency-main')]");
+                        var amount = currencyNode != null ? double.Parse($"{currencyNode.ChildNodes[0].InnerText}{currencyNode.ChildNodes[1].InnerText}".Replace(" ", "").Replace(",", ".")) : 0;
+                        var currency = currencyNode?.ChildNodes[2]?.InnerText;
+
+                        var dateTime = DateTime.Parse($"{date.Replace(",", "").Replace("года", "")} {time}", new CultureInfo("ru-RU"));
+                        listNodes.Add(new Operation() { DateTime = dateTime, Title = title, Place = place, Amount = amount, Currency = currency, Status = status });
+                    }
+                }
+
+                listNodes.Reverse();
+                result.AddRange(listNodes);
+            }
+            Console.WriteLine();
+
+            return result;
         }
     }
 
