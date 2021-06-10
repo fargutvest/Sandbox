d47c72a79db1d510bc8b5cd23c56a957489c77f8
Fargutwest <fargutvest@gmail.com>
2021-06-10 15:51:47 +0300

    Spring Boot Hello World +1

--- Java/Spring Boot/Hello World/Gradle/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java/Spring Boot/Hello World/Gradle/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "Super") String name) {
 		return String.format("Hello %s!", name);
 	}
 
