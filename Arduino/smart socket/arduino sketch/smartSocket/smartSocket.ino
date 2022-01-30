9a466da109141ff33699e65601c0ec091e763cd7
Farragut West <fargutvest@gmail.com>
2022-01-30 11:09:53 +0300

    fix necesserity click reset button
    Arduino/smart socket

--- Arduino/smart socket/arduino sketch/smartSocket/smartSocket.ino
+++ Arduino/smart socket/arduino sketch/smartSocket/smartSocket.ino
@@ -1,17 +1,16 @@
+int pinLevel = 255;
+
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
-int pinLevel = 255;
+
 
 void loop() {
   if (Serial.available() > 0){
-    pinLevel = Serial.read();  
-  }  
+  pinLevel = Serial.read(); 
+  } 
   digitalWrite(13, pinLevel); 
   Serial.write(pinLevel);
-  
-  while(Serial.available() == 0){
-   delay(10);
-  }   
+  delay(1000);
 }
