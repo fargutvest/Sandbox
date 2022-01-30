d6a2e84d78863dd2e97e59ee66fe6194cf403d96
Farragut West <fargutvest@gmail.com>
2022-01-30 10:10:52 +0300

    VS Code
    Arduino/smart socket

--- Arduino/smart socket/arduino sketch/smartSocket/smartSocket.ino
+++ Arduino/smart socket/arduino sketch/smartSocket/smartSocket.ino
@@ -1,15 +1,17 @@
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  
 }
 int pinLevel = 255;
 
 void loop() {
+  if (Serial.available() > 0){
+    pinLevel = Serial.read();  
+  }  
+  digitalWrite(13, pinLevel); 
   Serial.write(pinLevel);
+  
   while(Serial.available() == 0){
    delay(10);
-  }
-  pinLevel = Serial.read();
-  digitalWrite(13, pinLevel);    
+  }   
 }
