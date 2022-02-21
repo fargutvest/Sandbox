68a672ac067f0266488b93af8e4988c7a74474ff
Farragut West <fargutvest@gmail.com>
2022-02-21 17:32:05 +0300

    AsyncProgrammingAlexDevis Chapter1

---
+++ dotnet/AsyncProgrammingAlexDevis/AsyncProgrammingAlexDevis/Chapter1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace AsyncProgrammingAlexDevis
+{
+    public class Chapter1
+    {
+        public void DumpWebPage(string uri)
+        {
+            WebClient webClient = new WebClient();
+            string page = webClient.DownloadString(uri);
+            Console.WriteLine(page);
+        }
+
+        public async void DumpWebPageAsync(string uri)
+        {
+            WebClient webClient = new WebClient();
+            string page = await webClient.DownloadStringTaskAsync(uri);
+            Console.WriteLine(page);
+        }
+    }
+}
