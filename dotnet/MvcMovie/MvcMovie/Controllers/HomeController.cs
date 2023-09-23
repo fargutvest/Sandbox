72339c8451a5c06f9e49c28ea24f51b643c44d00
Farragut West <fargutvest@gmail.com>
2023-09-23 17:42:59 +0300

    Added MvcMovie prj

---
+++ dotnet/MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
