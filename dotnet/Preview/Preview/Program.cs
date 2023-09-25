1e796e1452d9c4e76887a84c26d4cbbac0efe956
Farragut West <fargutvest@gmail.com>
2023-09-25 11:57:55 +0300

    added preview proj

---
+++ dotnet/Preview/Preview/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+var target = Environment.GetCommandLineArgs()[1];
+
+var classesRoot = Registry.ClassesRoot;
+
+string perceivedType = "PerceivedType";
+string text = "text";
+
+
+if (classesRoot.GetSubKeyNames().Contains(target))
+{
+    var targetSk = classesRoot.OpenSubKey(target, true);
+    targetSk.SetValue(perceivedType, text);
+    Console.WriteLine($"Added value {perceivedType}={text} to subkey {target}");
+}
+else
+{
+    var targetSk = classesRoot.CreateSubKey(target, true);
+    targetSk.SetValue(perceivedType, text);
+    Console.WriteLine($"Created subkey {target} with value {perceivedType}={text}");
+}
